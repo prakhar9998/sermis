61bc6b1fd6b4715e6f9754006bfad659e15e3f55
Prakhar Varshney <prakhar.varshney33@gmail.com>
2019-05-11 21:10:32 +0530

    create models

--- forum/admin.py
+++ forum/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Forum, Topic, Post
 
-# Register your models here.
+admin.site.register(Forum)
+admin.site.register(Topic)
+admin.site.register(Post)
--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Forum(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(unique=True)
+    about = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
+class Topic(models.Model):
+    name = models.CharField(max_length=100)
+    forum = models.ForeignKey(
+        Forum,
+        on_delete=models.CASCADE,
+        related_name='topics'
+    )
+    created_by = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='topics'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_post_at = models.DateTimeField(null=True)
+    last_post_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Post(models.Model):
+    created_by = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_edited = models.DateTimeField(null=True)
+    topic = models.ForeignKey(
+        Topic,
+        on_delete=models.CASCADE,
+        related_name='topics'
+    )
+    content = models.TextField()
